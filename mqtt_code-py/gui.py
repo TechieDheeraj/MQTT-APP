d5fc185c11fba4a8699fc16116c7d3942b818315
Dheeraj <dheeraj.embdeveloper@gmail.com>
2018-12-04 02:03:53 +0530

    Changes For GUI

--- mqtt_code-py/gui.py
+++ mqtt_code-py/gui.py
@@ -6,36 +6,49 @@ window.geometry('350x200')
 window.title("Dheeraj Chat")
 
 lbl = Label(window, text = "WELCOME TO MY APP")
-lbl.grid(column=0, row=0)
-#lbl.pack()
+#lbl.grid(column=0, row=0)
+lbl.pack(side=TOP)
 
 messages_frame = Frame(window)
+messages_frame.pack(fill=X)
+
+
 #my_msg = StringVar()  # For the messages to be sent.
 #my_msg.set("Type your messages here.")
-scrollbar = Scrollbar(messages_frame)  # To navigate through past messages.
+scrollbar = Scrollbar(window)  # To navigate through past messages.
 
 def send(event):
 	print(entry_field.get())
-	lbl.configure(text = entry_field.get())
+	#lbl.configure(text = entry_field.get())
+	msg_list.insert(END, entry_field.get())
 
 ####
-#msg_list = Listbox(messages_frame, height=15, width=50, yscrollcommand=scrollbar.set)
-#scrollbar.pack(side=RIGHT, fill=Y)
-#msg_list.pack(side=LEFT, fill=BOTH)
+msg_list = Listbox(window, height=15, width=50, yscrollcommand=scrollbar.set)
+scrollbar.pack(side=RIGHT, fill=Y)
+#scrollbar.grid(column=4, row=1)
+msg_list.pack(side=TOP, fill=Y)
 ####
 
 #entry_field = Entry(window, textvariable=my_msg, width=30)
-entry_field = Entry(window, width=30)
+
+lbl1 = Label(messages_frame, text="Message Box", width=11)
+lbl1.pack(side=LEFT, padx=0, pady=0)
+
+entry_field = Entry(messages_frame, width=20)
+#entry_field = Entry(window, width=30)
 entry_field.bind("<Return>", send)
-entry_field.grid(column=0, row=2)
+#entry_field.grid(column=0, row=2)
 entry_field.focus()
+#entry_field.pack(side=TOP, fill=Y)
+entry_field.pack(side=LEFT, padx=5, fill=X)
 
 
 #btn = Button(window, text="send", command=send)
 btn = Button(window, text="send")
 btn.bind('<Button-1>', send)
+#btn.grid(column=2, row=2)
+btn.pack(side=RIGHT, fill=None)
 
-btn.grid(column=2, row=2)
 
 window.mainloop()
 
